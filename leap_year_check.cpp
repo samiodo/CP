e451e4e9016c7772902150b4dd4f80397f22037d
samiodo <93213020+samiodo@users.noreply.github.com>
2021-10-29 00:03:24 +0600

    Add files via upload

---
+++ leap_year_check.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int leap_year (int n) {
+    if (n % 400 == 0) return 1;
+    if (n % 100 == 0) return 0;
+    if (n % 4 == 0) return 1;
+    return 0;
+}
+
+int main () {
+    int n;
+    cin >> n;
+
+    leap_year(n) ? cout << "YES" : cout << "NO";
+}
